b355c8b4195d3e140084caafc1f39a737adc58da
jhp0415 <jh_p0415@naver.com>
2018-10-17 18:20:29 +0900

    Revert "fileIO"
    This reverts commit b688c8a396c777eee494455949cadbc32a78a4d2.

---
+++ fileIO/camera.py
@@ -0,0 +1,48 @@
+# camera.py
+
+import cv2
+
+class VideoCamera(object):
+    def __init__(self):
+        # Using OpenCV to capture from device 0. If you have trouble capturing
+        # from a webcam, comment the line below out and use a video file
+        # instead.
+        self.video = cv2.VideoCapture(0)
+        # If you decide to use video.mp4, you must have this file in the folder
+        # as the main.py.
+        # self.video = cv2.VideoCapture('video.mp4')
+
+    def __del__(self):
+        self.video.release()
+
+    def get_frame(self):
+        # Grab a single frame of video
+        ret, frame = self.video.read()
+        return frame
+
+    def get_jpg_bytes(self):
+        # Grab a single frame of video
+        success, frame = self.video.read()
+        # We are using Motion JPEG, but OpenCV defaults to capture raw images,
+        # so we must encode it into JPEG in order to correctly display the
+        # video stream.
+        ret, jpg = cv2.imencode('.jpg', frame)
+        return jpg.tobytes()
+
+
+if __name__ == '__main__':
+    cam = VideoCamera()
+    while True:
+        frame = cam.get_frame()
+
+        # show the frame
+        cv2.imshow("Frame", frame)
+        key = cv2.waitKey(1) & 0xFF
+
+        # if the `q` key was pressed, break from the loop
+        if key == ord("q"):
+            break
+
+    # do a bit of cleanup
+    cv2.destroyAllWindows()
+    print('finish')
